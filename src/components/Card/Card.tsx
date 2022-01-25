bbbb6a934d19335ffb784d8916eaae08959f1fc4
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-25 17:54:22 -0600

    ✨ Adding href to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,12 @@ import { FC } from 'react';
 import { Anime } from '../../interfaces/anime';
 import Image from 'next/image';
 import style from '../../../styles/Card.module.css';
+import Link from 'next/link';
 
 const Card: FC<{anime:Anime}> = ({ anime }) => {
   const getAiring = (): string => {
     const start = new Date(anime.aired.from);
     const end = new Date(anime.aired.to);
-    console.log(anime);
 
     if (end.toString() === 'Wed Dec 31 1969 18:00:00 GMT-0600 (Central Standard Time)' && anime.type === 'TV') {
       return `${start.toLocaleDateString("en-US")} - Present`;
@@ -18,39 +18,41 @@ const Card: FC<{anime:Anime}> = ({ anime }) => {
     
   }
   return (
-    <div className={`${style.flip}  p-5 bg-gray-300 rounded-md shadow drop-shadow-lg shadow-black`}>
-      <div className={style.flipContent}>
-        <div className={style.flipFront}>
-          <div className="w-full h-full relative">
-            <Image src={anime.images.webp.image_url} alt={`${anime.title} Poster`} layout="fill" priority />
-          </div>
-        </div>
-        <div className={style.flipBack}>
-          <div className="w-full h-full bg-gray-400 p-2 space-y-8 relative">
-            <h1 className="font-mochiy text-xl text-white text-center mt-4">{anime.title}</h1>
-            <div>
-              <p className="text-white font-sans text-sm mx-auto font-medium text-center">
-                {
-                  anime.status
-                }
-              </p>
-              <p className="text-white font-sans text-sm mx-auto font-medium text-center flex-wrap justify-center">
-                { getAiring() }
-              </p>
+    <Link href={`anime/${anime.mal_id}`}>
+      <a className={`${style.flip}  p-5 bg-gray-300 rounded-md shadow drop-shadow-lg shadow-black`}>
+        <div className={style.flipContent}>
+          <div className={style.flipFront}>
+            <div className="w-full h-full relative">
+              <Image src={anime.images.webp.image_url} alt={`${anime.title} Poster`} layout="fill" priority />
             </div>
-            <span className="flex items-baseline justify-center flex-wrap absolute bottom-2">
-              {
-                anime.genres.map((genre) => (
-                  <p key={genre.mal_id} className="bg-white rounded-full px-3 py-1 text-[10px] whitespace-nowrap text-gray-700 m-1">
-                    {genre.name}
-                  </p>
-                ))
-              }
-            </span>
+          </div>
+          <div className={style.flipBack}>
+            <div className="w-full h-full bg-gray-400 p-2 space-y-8 relative">
+              <h1 className="font-mochiy text-xl text-white text-center mt-4">{anime.title}</h1>
+              <div>
+                <p className="text-white font-sans text-sm mx-auto font-medium text-center">
+                  {
+                    anime.status
+                  }
+                </p>
+                <p className="text-white font-sans text-sm mx-auto font-medium text-center flex-wrap justify-center">
+                  { getAiring() }
+                </p>
+              </div>
+              <span className="flex items-baseline flex-row-reverse justify-center flex-wrap absolute bottom-2">
+                {
+                  anime.genres.map((genre) => (
+                    <p key={genre.mal_id} className="bg-white rounded-full px-3 py-1 text-[10px] whitespace-nowrap text-gray-700 m-1">
+                      {genre.name}
+                    </p>
+                  ))
+                }
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
     )
 }
 
