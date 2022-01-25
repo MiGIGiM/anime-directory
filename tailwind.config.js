3800958f4bcf49d15995a1702d3f900ec03e679f
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-25 11:03:33 -0600

    🔧 Configurating packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mochiy: ['"Mochiy Pop P One"', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
