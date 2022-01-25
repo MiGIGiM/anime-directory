ac941319f764b0058f7f44dd08aedcd01cda4bf6
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-25 17:57:22 -0600

    ✨ Adding anime details page

---
+++ pages/anime/[id].tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+const Anime: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div>
+      <h1> Details page for anime # {id}</h1>
+    </div>
+  );
+}
+
+export default Anime;
