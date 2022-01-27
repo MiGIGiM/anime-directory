e6b380e8df8f6c9568a9b6d0e8dc24daf9b9bd88
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-27 14:54:57 -0600

    💄 Fixing card styling

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
-// import Link from 'next/link';
+import Link from 'next/link';
 import Image from 'next/image';
 import { Anime } from '../../interfaces/anime';
 
-const Card: FC<{ anime: Anime }> = ({ anime }) => {
-    return (
-        <div className="w-56 h-96 relative">
-            <p>{anime.posterImage.original}</p>
-        </div>
-    );
-};
+const Card: FC<{ anime: Anime; id: string | number }> = ({ anime, id }) => (
+    <Link href={`/anime/${id}`}>
+        <a className="w-56 h-96 bg-blue-300 m-auto relative rounded-md shadow-black shadow-lg">
+            <div className="absolute z-10 bottom-3 backdrop-brightness-95 backdrop-blur">
+                <h1 className="text-gray-100 font-mochiy text-xl text-center p-4 w-56">{anime.titles.en_jp}</h1>
+            </div>
+            <Image src={anime.posterImage.original} layout="fill" className="z-0" priority />
+        </a>
+    </Link>
+);
 
 export default Card;
