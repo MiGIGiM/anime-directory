9f2eed544b58fd4cdde40e1f3411b26a37b248ad
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-28 16:49:55 -0600

    🎨 Improved styling structure

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,17 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { Anime } from '../../interfaces/anime';
 
 const Card: FC<{ anime: Anime; id: string | number }> = ({ anime, id }) => (
     <Link href={`/anime/${id}`}>
-        <a className="w-56 h-96 bg-blue-300 m-auto relative rounded-md shadow-black shadow-lg z-10">
-            <div className="absolute z-10 bottom-3 backdrop-brightness-95 backdrop-blur">
+        <a
+            className="w-56 h-96 bg-blue-300 m-auto rounded-md shadow-black shadow-lg bg-center bg-cover flex items-end py-5"
+            style={{ backgroundImage: `url(${anime.posterImage.original ?? anime.posterImage.medium})` }}
+        >
+            <div className="backdrop-brightness-95 backdrop-blur h-fit">
                 <h1 className="text-gray-100 font-mochiy text-xl text-center p-4 w-56">{anime.titles.en_jp}</h1>
             </div>
-            <Image
-                src={anime.posterImage.original ?? anime.posterImage.medium}
-                layout="fill"
-                className="z-0"
-                priority
-            />
         </a>
     </Link>
 );
