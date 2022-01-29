292ed8a3022407a57b4f5dbafd40a7c61a7c3abb
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-01-28 18:31:54 -0600

    ✨ Adding sorting functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import { useState, useEffect, Fragment } from 'react';
 import { Menu, Transition, Dialog } from '@headlessui/react';
 import { AdjustmentsIcon, SearchIcon } from '@heroicons/react/outline';
+import { ChevronDownIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
 import useAnimeList from '../src/hooks/useAnimeList';
 import Loader from '../src/components/SVGs/Search';
@@ -20,9 +21,11 @@ const Home: NextPage = () => {
     const closeModal = () => setIsOpen(false);
 
     const searchAnime = () => {
-        setAnimes([]);
-        setUrl(`https://kitsu.io/api/edge/anime?filter[text]=${query}&page%5Blimit%5D=20&page%5Boffset%5D=0`);
-        setQuery('');
+        if (query.length > 0) {
+            setAnimes([]);
+            setUrl(`https://kitsu.io/api/edge/anime?filter[text]=${query}&page%5Blimit%5D=20&page%5Boffset%5D=0`);
+            setQuery('');
+        }
     };
 
     const sortAnime = (sort: string) => {
@@ -35,6 +38,7 @@ const Home: NextPage = () => {
         setAnimes(newAnimes);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [animeList]);
+
     return (
         <div className="bg-[#f2efe7] py-5 px-2 xl:flex xl:flex-row-reverse items-start justify-center xl:space-y-0 space-y-10 min-h-screen">
             <nav
@@ -42,11 +46,11 @@ const Home: NextPage = () => {
                 transition-all delay-300 duration-300 ease-in-out shadow-black hover:drop-shadow-xl xl:block xl:w-3/12 xl:ml-20 sticky xl:top-10"
             >
                 <Link href="/">
-                    <a className="text-lg hover:opacity-30 text-gray-700 w-fit cursor-pointer md:text-2xl xl:mx-auto xl:text-3xl">
+                    <a className="text-lg hover:opacity-30 text-gray-700 w-fit cursor-pointer md:text-2xl xl:px-6 xl:text-3xl">
                         Anime Directory
                     </a>
                 </Link>
-                <div className="flex justify-evenly xl:hidden">
+                <div className="flex justify-evenly xl:hidden items-center space-x-6">
                     <button type="button" onClick={openModal}>
                         <SearchIcon className="w-5 h-5 md:w-8 mdh-8 focus:opacity-50 focus:text-slate-700" />
                     </button>
@@ -80,11 +84,12 @@ const Home: NextPage = () => {
                                         <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                                             Search Anime
                                         </Dialog.Title>
-                                        <div className="mt-2">
+                                        <div className="mt-2 space-y-4">
                                             <form
-                                                className="flex bg-gray-200 p-4 w-72 space-x-4 rounded-lg"
+                                                className="flex bg-gray-200 p-4 w-full space-x-4 rounded-lg items-center"
                                                 onSubmit={(e) => {
                                                     e.preventDefault();
+                                                    closeModal();
                                                     searchAnime();
                                                 }}
                                             >
@@ -99,19 +104,14 @@ const Home: NextPage = () => {
                                             </form>
                                             <button
                                                 type="submit"
-                                                className="bg-indigo-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg"
+                                                className="bg-indigo-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg mx-auto"
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    closeModal();
+                                                    searchAnime();
+                                                }}
                                             >
                                                 <span>Search</span>
-                                            </button>
-                                        </div>
-
-                                        <div className="mt-4">
-                                            <button
-                                                type="button"
-                                                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                                                onClick={closeModal}
-                                            >
-                                                Close
                                             </button>
                                         </div>
                                     </div>
@@ -211,7 +211,7 @@ const Home: NextPage = () => {
                         </Transition>
                     </Menu>
                 </div>
-                <div className=" hidden xl:flex items-center p-2 space-x-2 h-fit w-fit mx-auto my-5 font-mono">
+                <div className=" hidden xl:flex flex-col items-center p-2 space-x-2 h-fit w-fit mx-auto my-5 font-mono xl:space-x-0">
                     <form
                         className="flex bg-gray-200 p-4 w-72 space-x-4 rounded-lg"
                         onSubmit={(e) => {
@@ -219,7 +219,9 @@ const Home: NextPage = () => {
                             searchAnime();
                         }}
                     >
-                        <SearchIcon className="h-6 w-6 opacity-30" />
+                        <button type="submit">
+                            <SearchIcon className="h-6 w-6 opacity-30" />
+                        </button>
                         <input
                             className="bg-gray-200 outline-none"
                             type="text"
@@ -228,12 +230,98 @@ const Home: NextPage = () => {
                             onChange={(e) => setQuery(e.target.value)}
                         />
                     </form>
-                    <button
-                        type="submit"
-                        className="bg-indigo-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg hidden md:block"
-                    >
-                        <span>Search</span>
-                    </button>
+                    <Menu as="div" className="w-72">
+                        <div className="mt-5 bg-gray-200 p-4 w-full rounded-lg">
+                            <Menu.Button className="w-full text-right text-gray-700 inline-flex justify-between">
+                                Sort By
+                                <ChevronDownIcon className="h-6 w-6 opacity-30" />
+                            </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="absolute right-[5.5rem] z-50 text-sm font-sans w-56 flex flex-col justify-start space-y-3 p-3 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            type="button"
+                                            className={`${
+                                                active && 'bg-fuchsia-500 text-white font-semibold'
+                                            } rounded text-left p-3`}
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                sortAnime(
+                                                    'https://kitsu.io/api/edge/anime?filter[subtype]=movie&page%5Blimit%5D=20&page%5Boffset%5D=0'
+                                                );
+                                            }}
+                                        >
+                                            Show Movies
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            type="button"
+                                            className={`${
+                                                active && 'bg-sky-500 text-white font-semibold'
+                                            } rounded text-left p-3`}
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                sortAnime(
+                                                    'https://kitsu.io/api/edge/anime?filter[subtype]=tv&page%5Blimit%5D=20&page%5Boffset%5D=0'
+                                                );
+                                            }}
+                                        >
+                                            Show TV
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            type="button"
+                                            className={`${
+                                                active && 'bg-rose-500 text-white font-semibold'
+                                            } rounded text-left p-3`}
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                sortAnime(
+                                                    'https://kitsu.io/api/edge/anime?sort=-popularityRank&page%5Blimit%5D=20&page%5Boffset%5D=0'
+                                                );
+                                            }}
+                                        >
+                                            Sort by Popularity
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            type="button"
+                                            className={`${
+                                                active && 'bg-rose-500 text-white font-semibold'
+                                            } rounded text-left p-3`}
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                sortAnime(
+                                                    'https://kitsu.io/api/edge/anime?sort=slug&page%5Blimit%5D=20&page%5Boffset%5D=0'
+                                                );
+                                            }}
+                                        >
+                                            A - Z
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>
                 </div>
             </nav>
             {status !== 'loaded' && animes.length === 0 ? (
